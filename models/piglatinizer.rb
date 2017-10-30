2d180766cdd43adc9abf8543f91ac4ae37c45e6b
Richard Burd <richardburdx@yahoo.com>
2017-10-30 18:30:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,7 @@ class PigLatinizer
   def piglatinize(word)
 
     non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
-    vowels = ["a", "e", "i", "o", "u"]
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
     if non_pig_latin_words.include?(word)
       word
