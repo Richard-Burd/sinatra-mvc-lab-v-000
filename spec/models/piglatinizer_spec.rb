f6180559315e356b6939e6505d1c401f0df919c4
Richard Burd <richardburdx@yahoo.com>
2017-10-30 20:44:18 +0000

    automatically backed up by learn

--- spec/models/piglatinizer_spec.rb
+++ spec/models/piglatinizer_spec.rb
@@ -1,26 +1,26 @@
-describe 'class PigLatinizer' do
-  let!(:words) { PigLatinizer.new }
+describe 'class GoatAramaicizer' do
+  let!(:words) { GoatAramaicizer.new }
 
 
   it 'can create a new instance of a class' do
-    expect(PigLatinizer.new).to be_an_instance_of(PigLatinizer)
+    expect(GoatAramaicizer.new).to be_an_instance_of(PigLatinizer)
   end
 
   it 'piglatinizes an individual word' do
-    expect(words.piglatinize("pork")).to eq("orkpackh")
-    expect(words.piglatinize("I")).to eq("Itasah")
-    expect(words.piglatinize("hello")).to eq("ellohackh")
-    expect(words.piglatinize("please")).to eq("easeplackh")
-    expect(words.piglatinize("tomorrow")).to eq("omorrowtackh")
-    expect(words.piglatinize("until")).to eq("untiltasah")
-    expect(words.piglatinize("this")).to eq("isthackh")
-    expect(words.piglatinize("Enumeration")).to eq("Enumerationtasah")
-    expect(words.piglatinize("spray")).to eq("aysprackh")
-    expect(words.piglatinize("prays")).to eq("aysprackh")
+    expect(words.goataramaicize("pork")).to eq("orkpackh")
+    expect(words.goataramaicize("I")).to eq("Itasah")
+    expect(words.goataramaicize("hello")).to eq("ellohackh")
+    expect(words.goataramaicize("please")).to eq("easeplackh")
+    expect(words.goataramaicize("tomorrow")).to eq("omorrowtackh")
+    expect(words.goataramaicize("until")).to eq("untiltasah")
+    expect(words.goataramaicize("this")).to eq("isthackh")
+    expect(words.goataramaicize("Enumeration")).to eq("Enumerationtasah")
+    expect(words.goataramaicize("spray")).to eq("aysprackh")
+    expect(words.goataramaicize("prays")).to eq("aysprackh")
   end
 
   it 'has a method splits the sentence to piglatinize each word' do
-    expect(words.to_pig_latin("i love programming")).to eq("itasah ovelackh ogrammingprackh")
+    expect(words.to_goat_aramaic("i love programming")).to eq("itasah ovelackh ogrammingprackh")
   end
 
 end
